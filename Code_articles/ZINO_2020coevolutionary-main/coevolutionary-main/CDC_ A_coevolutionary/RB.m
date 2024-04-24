b1fe139545e7c9a3092e8bd2df15a2a44741a0ff
Riccardo <101208642+rktessa@users.noreply.github.com>
2024-04-24 09:18:13 +0200

    Delete Code_articles directory

--- Code_articles/ZINO_2020coevolutionary-main/coevolutionary-main/CDC_ A_coevolutionary/RB.m
+++
@@ -1,9 +0,0 @@
-function c=RB(x)
-% RB(x,max,min) gives an rgb color in a red-blue scale, depending on the value x and on max and min
-
-if x>=.5
-        c=[2-2*x,2-2*x,1];
-else
-    c=[1,2*x,2*x];
-end
-end
