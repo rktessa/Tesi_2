2263fb05cd1bc612b34af070995e3093513a3917
rktessa <rktessa@gmail.com>
2024-09-05 18:23:27 +0200

    new review

--- Matlab/D_multilayer_system/figura_worst_epidemic.m
+++ Matlab/D_multilayer_system/figura_worst_epidemic.m
@@ -11,17 +11,17 @@ x_year = [730 800 850 1146 1220 1288 1325 1400 1430 1540 1630 1665 1700 1750 175
 epidemic_deaths = [7.5,50,2, 50, 7, 16, 2.5, 1, 1.25, 2, 1, 15,1, 50,3, 2, 2, 43, 20 ];
 y_deaths = [7.5 50 2 50 7 16 2.5 1 1.25 2 1 15 1 50 3 2 2 43 20];
 
-epidemic_name = {' Antonine Plague', ' Plague of Justinian', ' Japanese smallpox', ' Black death', ' Mexico smallpox', 'Cocoliztli epidemic',...
-    ' Cocoliztli epidemic II',' Italian plague', ' Naples plague', ' Persian plague', ' Cholera', ' The third plague', 'Influenza',...
-    ' Spanish Flu', ' Russian typhus',' Influenza pandemic', ' Hong Kong flu', ' HIV/AIDS', ' Covid-19' };
+epidemic_name = {'  Antonine Plague', '    Plague of Justinian', '    Japanese smallpox', '    Black death', '    Mexico smallpox', '    Cocoliztli epidemic',...
+    '    Cocoliztli epidemic II','   Italian plague', '    Naples plague', '    Persian plague', '    Cholera', '    The third plague', '    Influenza',...
+    '    Spanish Flu', '    Russian typhus','    Influenza pandemic', '    Hong Kong flu', '    HIV/AIDS', '    Covid-19' };
 
 figure(1)
 sz = 25;
 c = linspace(1,10,length(year));
 scatter(year,epidemic_deaths,sz,c,'filled')
 box on
-ylim([0, 70])
-xlim([695, 2055])
+ylim([0, 85])
+xlim([695, 2075])
 xticks(x_year)
 xticklabels({'165', '541', '735', '1346', '1520', '1548', '1570', '1629', '1656', '1772', '1846', '1855', '1889', '1918', '', '1957', '1968', '1981', '2019' })
 xtickangle(70)
